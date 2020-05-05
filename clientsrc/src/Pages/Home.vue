6d9415ddf4aad27288a55b38a2031cdc3e3a2a20
Brandon Reninger <brandonreninger@yahoo.com>
2020-05-05 10:03:39 -0600

    hard coded donation working

--- clientsrc/src/Pages/Home.vue
+++ clientsrc/src/Pages/Home.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="home container-fluid">
-
-
-
     <div class="row m-3">
       <div class="col-6 ml-5">
         <form class="form-inline my-2 my-lg-0">
@@ -20,25 +17,19 @@
           >Search</button>
         </form>
       </div>
-        <div>
-          <button class="btn btn-sm btn-primary" @click="next()">NEXT</button>
-        </div>
+      <div>
+        <button class="btn btn-sm btn-primary" @click="next()">NEXT</button>
+      </div>
     </div>
 
-
     <div class="card bg-dark text-white">
-  <img :src="orgData.picture" class="card-img" alt="">
-  <div class="card-img-overlay text-center text-dark">
-    <h1 class="card-title">{{orgData.name}}</h1>
-  </div>
-</div>
+      <img :src="orgData.picture" class="card-img" alt />
+      <div class="card-img-overlay text-center text-dark">
+        <h1 class="card-title">{{orgData.name}}</h1>
+      </div>
+    </div>
 
-
-
-
-
-
-<!-- <div class="row">
+    <!-- <div class="row">
 
       <div class="col-12 text-center">
         <img class="img-fluid w-80" :src="orgData.picture" />
@@ -50,13 +41,13 @@
       <div class="col-12">
         <h1>{{orgData.name}}</h1>
       </div>
-    </div>  -->
-
+    </div>-->
 
     <div class="row text-center m-3">
       <div class="col-6 offset-3">
         <button class="btn btn-block btn-primary" @click="donate()">Donate</button>
       </div>
+      <div ref="paypal"></div>
     </div>
     <hr />
     <org-details></org-details>
@@ -71,7 +62,9 @@ export default {
   data() {
     return {
       search: "",
-      searchLocations: ""
+      searchLocations: "",
+      loaded: false,
+      product: { price: 5, description: "You made a donation" }
     };
   },
   computed: {
@@ -92,28 +85,34 @@ export default {
       return this.$store.state.orgs;
     },
     filteredList() {
-      let out = []
+      let out = [];
       let list = this.$store.state.orgs;
 
-      for (let i =0; i < list.length; i++){
-        let listItem = list[i]
+      for (let i = 0; i < list.length; i++) {
+        let listItem = list[i];
         // if (listItem.name.toLowerCase().includes(this.search.toLowerCase()) &&
         //  listItem.address.toLowerCase().includes(this.searchLocations.toLowerCase()) ){
         //    out.push(listItem)
         // }
-        if (listItem.name.toLowerCase().includes(this.search.toLowerCase()) ||
-         listItem.address.toLowerCase().includes(this.search.toLowerCase()) ){
-           out.push(listItem)
+        if (
+          listItem.name.toLowerCase().includes(this.search.toLowerCase()) ||
+          listItem.address.toLowerCase().includes(this.search.toLowerCase())
+        ) {
+          out.push(listItem);
         }
       }
-      return out
-
+      return out;
     }
   },
   mounted() {
     //debugger
     this.$store.dispatch("getOrgs");
     //this.$store.dispatch("next", 0);
+    const script = document.createElement("script");
+    script.src =
+      "https://www.paypal.com/sdk/js?client-id=AXVku1rBN3Z1MbP9hLn_3u3PnILdDpe_iG5CWzvzgYfuyMATqQ-hybUMbn33mmFH041mm7lGMkJsWkK6";
+    script.addEventListener("load", this.setLoaded);
+    document.body.appendChild(script);
   },
   methods: {
     next() {
@@ -143,6 +142,26 @@ export default {
       console.log("searchOrgs", this.filteredList);
       console.log("searchOrgs", this.filteredList[0]);
       this.$store.commit("setActiveOrg", this.filteredList[0]);
+    },
+    setLoaded: function() {
+      this.loaded = true;
+      window.paypal
+        .Buttons({
+          createOrder: (data, actions) => {
+            return actions.order.create({
+              purchase_units: [
+                {
+                  description: "this is a donation",
+                  amount: {
+                    currency_code: "USD",
+                    value: this.product.price
+                  }
+                }
+              ]
+            });
+          }
+        })
+        .render(this.$refs.paypal);
     }
   },
   components: { OrgDetails }
